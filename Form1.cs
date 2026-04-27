c03274e0c7876cb07cb478921242ae9299c72f4b
Manarihab <manarihab5@gmail.com>
2026-04-27 19:19:09 +0300

    Added Phase 2 (Parser)

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Data;
 using System.Text.RegularExpressions;
 
@@ -23,21 +24,23 @@ namespace compiler_project
             string identifiers = @"\b[a-zA-Z][a-zA-Z0-9]*\b"; 
             string numbers = @"\b\d+(\.\d+)?\b"; 
             string strings = "\"[^\"]*\""; 
-            string functionCalls = @"\b[a-zA-Z][a-zA-Z0-9]*\s*\([^)]*\)"; 
             string comments = @"/\*.*?\*/"; 
             string arethmatic_operators = @"[+\-*/]"; 
             string assignment_operators = @":=";
-            string symbols = @"[;(),]";
-            string term = $@"({functionCalls}|{numbers}|{identifiers})";
-            string masterPattern = $"{comments}|{strings}|{functionCalls}|{arethmatic_operators}|{symbols}|{identifiers}|{numbers}|{keywords}";
+            string relational_operators = @"(<=|>=|<>|<|>|=)";
+            string boolean_operators = @"(\|\||&&)";
+            string symbols = @"[;(),{}]";
+            string term = $@"({numbers}|{identifiers})";
+            string masterPattern = $"{comments}|{strings}|{assignment_operators}|{relational_operators}|{boolean_operators}|{arethmatic_operators}|{symbols}|{identifiers}|{numbers}|{keywords}";
 
 
             DataTable dt = new DataTable();
             dt.Columns.Add("Lexeme");
             dt.Columns.Add("Token Type");
-
+            
 
             MatchCollection matches = Regex.Matches(input, masterPattern);
+            List<Token> allTokens = new List<Token>();
             foreach (Match m in matches)
             {
                 string lex = m.Value;
@@ -48,8 +51,7 @@ namespace compiler_project
                     type = "Comment_Statement";
                 else if (Regex.IsMatch(lex, strings))
                     type = "String";
-                else if (Regex.IsMatch(lex, functionCalls))
-                    type = "Function_Call";
+                
                 else if (Regex.IsMatch(lex, keywords)) { 
                     if (lex == "int" || lex == "float" || lex == "string")
                         type = "Datatype"; 
@@ -62,6 +64,14 @@ namespace compiler_project
                     type = "Number";
                 else if (Regex.IsMatch(lex, identifiers))
                     type = "Identifier";
+                else if (Regex.IsMatch(lex, assignment_operators))
+                    type = ":=";
+
+                else if (Regex.IsMatch(lex, relational_operators))
+                    type = "RelOp";
+
+                else if (Regex.IsMatch(lex, boolean_operators))
+                    type = "BoolOp";
                 else if (Regex.IsMatch(lex, arethmatic_operators))
                 {
                     switch (lex)
@@ -88,8 +98,22 @@ namespace compiler_project
                     type = "Unknown";
 
                 dt.Rows.Add(lex, type);
+                allTokens.Add(new Token { Value = lex, Type = type });
             }
             dataGridView1.DataSource = dt;
+            try
+            {
+                if (allTokens.Count == 0) return;
+                TinyParser parser = new TinyParser(allTokens);
+                parser.ParseProgram();
+                MessageBox.Show(" Success: Your TinyLanguage code is correct!", " Success" ,
+MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Syntax Error: " +ex.Message, " Error",
+MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
---
+++ TinyParser.cs
@@ -0,0 +1,389 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace compiler_project
+{
+    public class TinyParser
+    {
+        private readonly List<Token> tokens;
+        private int index = 0;
+        private Token currentToken;
+
+        public TinyParser(List<Token> tokens)
+        {
+            this.tokens = tokens;
+            if (tokens.Count > 0)
+                currentToken = tokens[0];
+        }
+
+        
+        private void Match(string expected)
+        {
+            if (index < tokens.Count &&
+                (currentToken.Value == expected || currentToken.Type == expected))
+            {
+                index++;
+                currentToken = index < tokens.Count ? tokens[index] : null;
+            }
+            else
+            {
+                throw new Exception(
+                    $"Expected '{expected}' but found '{currentToken?.Value}'"
+                );
+            }
+        }
+
+        private Token LookAhead(int k)
+        {
+            return (index + k < tokens.Count) ? tokens[index + k] : null;
+        }
+
+        private bool IsDatatype()
+        {
+            return currentToken?.Value == "int" ||
+                   currentToken?.Value == "float" ||
+                   currentToken?.Value == "string";
+        }
+
+        private void ParseDatatype()
+        {
+            if (IsDatatype())
+                Match(currentToken.Value);
+            else
+                throw new Exception("Expected datatype");
+        }
+
+        
+        public void ParseProgram()
+        {
+            while (index < tokens.Count)
+            {
+                if (IsDatatype())
+                {
+                    
+                    if (LookAhead(1)?.Value == "main")
+                    {
+                        ParseMainFunction();
+                    }
+                    
+                    else if (LookAhead(2)?.Value == "(")
+                    {
+                        ParseFunction();
+                    }
+                    
+                    else
+                    {
+                        ParseDeclaration();
+                    }
+                }
+                else
+                {
+                    throw new Exception(
+                        $"Program must start with datatype or function. Found '{currentToken.Value}'"
+                    );
+                }
+            }
+        }
+
+        
+        private void ParseFunction()
+        {
+            ParseDatatype();
+            Match("Identifier");
+            Match("(");
+            ParseParameters();
+            Match(")");
+            ParseFunctionBody();
+        }
+
+        private void ParseMainFunction()
+        {
+            ParseDatatype();
+
+            if (currentToken.Value == "main")
+                Match("main");
+            else
+                throw new Exception("Expected 'main'");
+
+            Match("(");
+            Match(")");
+            ParseFunctionBody();
+        }
+
+        private void ParseParameters()
+        {
+            if (IsDatatype())
+            {
+                ParseDatatype();
+                Match("Identifier");
+
+                while (currentToken.Value == ",")
+                {
+                    Match(",");
+                    ParseDatatype();
+                    Match("Identifier");
+                }
+            }
+        }
+
+        private void ParseFunctionBody()
+        {
+            Match("{");
+            ParseStatements();
+            ParseReturn();
+            Match("}");
+        }
+
+        
+        private void ParseStatements()
+        {
+            while (index < tokens.Count &&
+                   currentToken.Value != "}" &&
+                   currentToken.Value != "end" &&
+                   currentToken.Value != "until" &&
+                   currentToken.Value != "return")   
+            {
+                ParseStatement();
+            }
+        }
+
+        private void ParseStatement()
+        {
+            if (IsDatatype())
+                ParseDeclaration();
+
+            else if (currentToken.Type == "Identifier")
+                ParseAssignment();
+
+            else if (currentToken.Value == "read")
+                ParseRead();
+
+            else if (currentToken.Value == "write")
+                ParseWrite();
+
+            else if (currentToken.Value == "return")
+                ParseReturn();
+
+            else if (currentToken.Value == "if")
+                ParseIf();
+
+            else if (currentToken.Value == "repeat")
+                ParseRepeat();
+
+            else
+                throw new Exception(
+                    $"Invalid statement start: '{currentToken.Value}'"
+                );
+        }
+
+        
+        private void ParseDeclaration()
+        {
+            ParseDatatype();
+            ParseDeclItem();
+
+            while (currentToken.Value == ",")
+            {
+                Match(",");
+                ParseDeclItem();
+            }
+
+            Match(";");
+        }
+
+        private void ParseDeclItem()
+        {
+            Match("Identifier");
+
+            if (currentToken.Value == ":=")
+            {
+                Match(":=");
+                ParseExpression();
+            }
+        }
+
+        
+        private void ParseAssignment()
+        {
+            Match("Identifier");
+            Match(":=");
+            ParseExpression();
+            Match(";");
+        }
+
+        
+        private void ParseRead()
+        {
+            Match("read");
+            Match("Identifier");
+            Match(";");
+        }
+
+        private void ParseWrite()
+        {
+            Match("write");
+
+            if (currentToken.Value == "endl")
+                Match("endl");
+            else
+                ParseExpression();
+
+            Match(";");
+        }
+
+        
+        private void ParseReturn()
+        {
+            Match("return");
+            ParseExpression();
+            Match(";");
+        }
+
+        
+        private void ParseIf()
+        {
+            Match("if");
+            ParseConditionStatement();
+            Match("then");
+
+            ParseStatements();
+
+            while (currentToken.Value == "elseif")
+            {
+                Match("elseif");
+                ParseConditionStatement();
+                Match("then");
+                ParseStatements();
+            }
+
+            if (currentToken.Value == "else")
+            {
+                Match("else");
+                ParseStatements();
+            }
+
+            Match("end");
+        }
+
+        
+        private void ParseRepeat()
+        {
+            Match("repeat");
+            ParseStatements();
+            Match("until");
+            ParseConditionStatement();
+        }
+
+        
+        private void ParseConditionStatement()
+        {
+            ParseCondition();
+
+            while (currentToken.Value == "&&" || currentToken.Value == "||")
+            {
+                Match(currentToken.Value);
+                ParseCondition();
+            }
+        }
+
+        private void ParseCondition()
+        {
+            Match("Identifier");
+
+            if (currentToken.Value == "<" ||
+                currentToken.Value == ">" ||
+                currentToken.Value == "=" ||
+                currentToken.Value == "<>")
+            {
+                Match(currentToken.Value);
+            }
+            else
+            {
+                throw new Exception("Expected condition operator");
+            }
+
+            ParseTerm();
+        }
+
+        
+        private void ParseExpression()
+        {
+            if (currentToken.Type == "String")
+            {
+                Match("String");
+                return;
+            }
+
+            ParseEquation();
+        }
+
+        private void ParseEquation()
+        {
+            ParseTerm();
+
+            while (currentToken.Value == "+" || currentToken.Value == "-")
+            {
+                Match(currentToken.Value);
+                ParseTerm();
+            }
+        }
+
+        private void ParseTerm()
+        {
+            ParseFactor();
+
+            while (currentToken.Value == "*" || currentToken.Value == "/")
+            {
+                Match(currentToken.Value);
+                ParseFactor();
+            }
+        }
+
+        private void ParseFactor()
+        {
+            if (currentToken.Type == "Number")
+            {
+                Match("Number");
+            }
+            else if (currentToken.Type == "Identifier")
+            {
+                Match("Identifier");
+
+               
+                if (currentToken?.Value == "(")
+                {
+                    Match("(");
+                    ParseArguments();
+                    Match(")");
+                }
+            }
+            else if (currentToken.Value == "(")
+            {
+                Match("(");
+                ParseEquation();
+                Match(")");
+            }
+            else
+            {
+                throw new Exception($"Unexpected token '{currentToken.Value}'");
+            }
+        }
+
+        private void ParseArguments()
+        {
+            if (currentToken.Type == "Identifier")
+            {
+                Match("Identifier");
+
+                while (currentToken.Value == ",")
+                {
+                    Match(",");
+                    Match("Identifier");
+                }
+            }
+        }
+    }
+}
